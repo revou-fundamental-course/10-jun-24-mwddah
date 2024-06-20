addc23c49f2e9344393fe00b391320a03704c3bf
mwddah <mwddhst@gmail.com>
2024-06-20 14:08:12 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,27 +1,21 @@
+// script.js
+
 function convertToFahrenheit() {
-    const celcius = parseFloat(document.getElementById('celciusInput').value);
-    if (isNaN(celcius)) {
-        alert('Masukkan nilai derajat Celcius yang valid.');
-        return;
-    }
-    const fahrenheit = (celcius * 9/5) + 32;
-    document.getElementById('fahrenheitInput').value = fahrenheit.toFixed(2);
-    document.getElementById('calculationSteps').value = `${celcius}°C x 9/5 + 32 = ${fahrenheit.toFixed(2)}°F`;
+    const celciusInput = document.getElementById("celciusInput").value;
+    const fahrenheitResult = (parseFloat(celciusInput) * 9/5) + 32;
+    document.getElementById("fahrenheitInput").value = fahrenheitResult.toFixed(2);
+    document.getElementById("calculationSteps").value = `(${celciusInput} x 9/5) + 32 = ${fahrenheitResult.toFixed(2)}`;
 }
 
 function resetFields() {
-    document.getElementById('celciusInput').value = '';
-    document.getElementById('fahrenheitInput').value = '';
-    document.getElementById('calculationSteps').value = '';
+    document.getElementById("celciusInput").value = "";
+    document.getElementById("fahrenheitInput").value = "";
+    document.getElementById("calculationSteps").value = "";
 }
 
 function reverseConversion() {
-    const fahrenheit = parseFloat(document.getElementById('fahrenheitInput').value);
-    if (isNaN(fahrenheit)) {
-        alert('Masukkan nilai derajat Fahrenheit yang valid.');
-        return;
-    }
-    const celcius = (fahrenheit - 32) * 5/9;
-    document.getElementById('celciusInput').value = celcius.toFixed(2);
-    document.getElementById('calculationSteps').value = `${fahrenheit}°F - 32 x 5/9 = ${celcius.toFixed(2)}°C`;
+    const fahrenheitInput = document.getElementById("fahrenheitInput").value;
+    const celciusResult = (parseFloat(fahrenheitInput) - 32) * 5/9;
+    document.getElementById("celciusInput").value = celciusResult.toFixed(2);
+    document.getElementById("calculationSteps").value = `(${fahrenheitInput} - 32) x 5/9 = ${celciusResult.toFixed(2)}`;
 }
